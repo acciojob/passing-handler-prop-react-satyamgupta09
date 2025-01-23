21a4650135eacd8572ba9112a60b937ce8508985
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-24 02:04:22 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,22 @@
 
+import React, { useState } from "react";
+
+const Selection = ({ applyColor }) => {
+  const [background, setBackground] = useState("");
+
+  const updateSelectionStyle = (newBackground) => {
+    setBackground(newBackground.background);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{ background: background }}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      <p className="center">Click me to change color</p>
+    </div>
+  );
+};
+
+export default Selection;
